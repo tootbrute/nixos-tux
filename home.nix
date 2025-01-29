3bcbeb4fca869618959bca294c716618a6f9d4b2
Tootbrute <tootbrute@tutamail.com>
2025-01-29 23:45:37 +0800

    fiddled with GNOME extensions

--- home.nix
+++ home.nix
@@ -9,6 +9,7 @@
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     neovim
+    gnome-tweaks
   ];
 
   # Environment
--- mods/gnome.nix
+++ mods/gnome.nix
@@ -34,15 +34,13 @@
     gnome-tour
   ];
 
-  environment.systemPackages = with pkgs; [
-    #Gnome tweak tools
-    gnome-tweaks
-    gnomeExtensions.appindicator
-    gnomeExtensions.caffeine
-    gnomeExtensions.gsconnect
-    gnomeExtensions.hot-edge
-    gnomeExtensions.alphabetical-app-grid
-    gnomeExtensions.kimpanel
+  environment.systemPackages = with pkgs.gnomeExtensions; [
+    appindicator
+    caffeine
+    gsconnect
+    hot-edge
+    alphabetical-app-grid
+    kimpanel
   ];
 
 }
