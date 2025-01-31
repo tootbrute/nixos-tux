2adb3c904aac428e6475550c0c64c98037ab03bd
Tootbrute <tootbrute@tutamail.com>
2025-01-31 21:05:49 +0800

    adjusted locale

--- mods/base.nix
+++ mods/base.nix
@@ -54,7 +54,7 @@
   time.timeZone = "Asia/Taipei";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_CA.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
   i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" "zh_CN.UTF-8/UTF-8" "zh_TW.UTF-8/UTF-8" ];
 
   i18n.extraLocaleSettings = {
