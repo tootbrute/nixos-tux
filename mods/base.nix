cd957084b055bbafb31ed8b27564a334a4a4e71a
Tootbrute <tootbrute@tutamail.com>
2025-02-01 17:19:54 +0800

    trying to fix locale but still have error

--- mods/base.nix
+++ mods/base.nix
@@ -61,6 +61,7 @@
     # added LANG, LC_ALL, LC_CTYPE, LC_COLLATE, and LC_MESSAGES
     # to get rid of perl locale error
     LANGUAGE = "en_US.UTF-8";
+    LANG = "en_US.UTF-8";
     LC_ALL = "en_US.UTF-8";
     LC_CTYPE = "en_US.UTF-8";
     LC_COLLATE = "en_US.UTF-8";
