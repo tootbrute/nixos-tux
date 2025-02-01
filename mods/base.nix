2e786b20bbaee35fe52af3909a934d1f97ab51a6
Tootbrute <tootbrute@tutamail.com>
2025-02-01 17:05:34 +0800

    trying to fix up locale errors

--- mods/base.nix
+++ mods/base.nix
@@ -55,7 +55,7 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" "zh_CN.UTF-8/UTF-8" "zh_TW.UTF-8/UTF-8" ];
+  i18n.supportedLocales = [ "en_CA.UTF-8/UTF-8" "zh_CN.UTF-8/UTF-8" "zh_TW.UTF-8/UTF-8" ];
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
